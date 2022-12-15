98599f1d03687d87eea96ca8260a051478a4be22
TacticalCoders <docdoca@naver.com>
2022-12-15 17:57:03 +0900

    Initializing by random, one, zero

--- THE BASICS/tensor/initialization.py
+++ THE BASICS/tensor/initialization.py
@@ -9,3 +9,13 @@ data = [[1, 2], [3, 4]]
 x_data = torch.tensor(data)  # convert list to tensor
 
 print(x_data)
+
+# Tensors can be initialized randomly or by one, zero
+shape = (2, 3,)  # shape is a tuple of tensor dimensions.
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")  # f can print variable between {}
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
