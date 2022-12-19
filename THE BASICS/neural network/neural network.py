3f945ec2023a042842c627dbfe63034088f1f1e5
TacticalCoders <docdoca@naver.com>
2022-12-19 18:58:29 +0900

    make custom neural network

---
+++ THE BASICS/neural network/neural network.py
@@ -0,0 +1,37 @@
+"""
+신경망 구성 하기
+"""
+
+# torch.nn provides all building blocks you need
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+
+# Every module in pytorch subclasses the nn.Module
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()  # have to init superclass first
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    # every nn.Module subclass implements forward method.
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+model = NeuralNetwork().to(device)
+print(model)
